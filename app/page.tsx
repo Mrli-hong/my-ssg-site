53fb8ec706ca3d46ea4721c5920f15cb9d43b134
HS\lihy49525 <lihy49525@hundsun.com>
2025-05-27 13:44:24 +0800

    vvvv

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div className="text-sm mb-4">{time}</div>
+      <div className="text-sm mb-4">{time}lhy</div>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"

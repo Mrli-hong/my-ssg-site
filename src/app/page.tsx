ea7e50f579e7caceb2897a93140d35ca048c3141
HS\lihy49525 <lihy49525@hundsun.com>
2025-05-27 10:23:25 +0800

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const time = new Date().toISOString();
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="text-sm mb-4">{time}</div>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
